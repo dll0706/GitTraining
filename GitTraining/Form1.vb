dc64ce0e2d9b4fde8273593db74de05bce4d2fa8
dll0706 <dave@scribblygum.net.au>
2018-02-14 15:45:50 +1300

    Major changes

--- GitTraining/Form1.vb
+++ GitTraining/Form1.vb
@@ -1,14 +1,14 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Label1.Text = "Hello World"
+        Label1.Text = Button1.Tag
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Label1.Text = "QSL Rocks!"
+        Label1.Text = Button2.Tag
     End Sub
 
-    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
-        Label1.Text = "Time to party!"
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.Text = Button3.Tag
     End Sub
 End Class

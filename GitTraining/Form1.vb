5b38bc0edb5c3fd716f89ba7833c9615b08be92c
dll0706 <dave@scribblygum.net.au>
2018-02-14 15:17:50 +1300

    First ever release

--- GitTraining/Form1.vb
+++ GitTraining/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Text = "Hello World"
     End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        Label1.Text = "Time to party!"
+    End Sub
 End Class

e601a46d9a90c6940ce6e57bc13989e3c0b90d4d
dll0706 <dave@scribblygum.net.au>
2018-02-14 15:14:04 +1300

    Added message 2

--- GitTraining/Form1.vb
+++ GitTraining/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Text = "Hello World"
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "QSL Rocks!"
+    End Sub
 End Class
